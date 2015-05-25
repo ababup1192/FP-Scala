9e2a7fa155a2401dda55922bba6dc9727dec0b60
ababup1192 <ababup1192@gmail.com>
2015-05-25 15:54:18 +0900

    Implement functions of Option

--- src/main/scala/org/ababup1192/Option.scala
+++ src/main/scala/org/ababup1192/Option.scala
@@ -1,7 +1,25 @@
 package org.ababup1192
 
 
-sealed trait Option[+A]
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = {
+    this match {
+      case Some(a) => Some(f(a))
+      case None => None
+    }
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    this.map(f(_)).getOrElse(None)
+  }
+
+  def getOrElse[B >: A](default: => B): B = {
+    this match {
+      case Some(a) => a
+      case None => default
+    }
+  }
+}
 
 case class Some[+A](get: A) extends Option[A]
 
--- src/test/scala/org/ababup1192/OptionSpec.scala
+++ src/test/scala/org/ababup1192/OptionSpec.scala
@@ -10,6 +10,27 @@ class OptionSpec extends FlatSpec with Matchers {
     mean(Seq(1.0, 2.0, 3.0)) should be(Some(2.0))
   }
 
+  "The map function" should "return a new Option value" in {
+    def overTen(n: Int) = if (n > 10) Some(n) else None
+
+    overTen(11).map(_ * 2) should be(Some(22))
+    overTen(5).map(_ * 2) should be(None)
+  }
+
+  "The flatMap function" should "return a new Option value" in {
+    def overTen(n: Int) = if (n > 10) Some(n) else None
+
+    overTen(11).flatMap(x => Some(x * 2)) should be(Some(22))
+    overTen(5).flatMap(x => Some(x * 2)) should be(None)
+  }
+
+  /*
+  "The flatMap function" should "return a new Option value" in {
+    Some(10).map(_ * 2) should be(Some(20))
+  }
+  */
+
+
 }
 
 
