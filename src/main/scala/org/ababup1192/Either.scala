7e541ac7b6842a6133604c33584282c0139acfc8
ababup1192 <ababup1192@gmail.com>
2015-05-26 09:25:59 +0900

    Implement functions of Either

--- src/main/scala/org/ababup1192/Either.scala
+++ src/main/scala/org/ababup1192/Either.scala
@@ -1,6 +1,35 @@
 package org.ababup1192
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = {
+    this match {
+      case l: Left[E] => l
+      case Right(a) => Right(f(a))
+    }
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+    this match {
+      case l: Left[E] => l
+      case Right(a) => f(a)
+    }
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
+    this match {
+      case _: Left[E] => b
+      case r: Right[_] => r
+    }
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    for {
+      va <- this
+      vb <- b
+    } yield f(va, vb)
+  }
+
+}
 
 case class Left[+E](value: E) extends Either[E, Nothing]
 
