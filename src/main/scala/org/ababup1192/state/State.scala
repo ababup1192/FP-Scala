925462905f7040c38d4932f76bad99c3b66c8a21
ababup1192 <ababup1192@gmail.com>
2015-05-31 10:00:29 +0900

    WEP

--- src/main/scala/org/ababup1192/state/State.scala
+++ src/main/scala/org/ababup1192/state/State.scala
@@ -1,6 +1,11 @@
 package org.ababup1192.state
 
 trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+
   type Rand[+A] = RNG => (A, RNG)
 
   def unit[A](a: A): Rand[A] = rng => (a, rng)
@@ -12,56 +17,60 @@ trait RNG {
     }
   }
 
-  def nextInt: (Int, RNG)
+  case class SimpleRNG(seed: Long) extends RNG {
+    override def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+
+    val int: Rand[Int] = _.nextInt
+
+    def nonNegativeInt: Rand[Int] = {
+      map(int)(i => if (i < 0) -(i + 1) else i)
+    }
+
+    def nonNegativeEven: Rand[Int] = {
+      map(nonNegativeInt)(i => i - i % 2)
+    }
+
+    def double: Rand[Double] = {
+      val (n, r) = nonNegativeInt(this)
+      rng: RNG => (n / (Int.MaxValue.toDouble + 2), r)
+    }
+
+    def intDouble(rng: RNG): ((Int, Double), RNG) = {
+      val (i, r) = rng.nextInt
+      val (d, r2) = double(r)
+      (i -> d, r2)
+    }
+
+    def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+      val (v, r) = intDouble(rng)
+      (v.swap, r)
+    }
+
+    def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+      val (d1, r1) = double(rng)
+      val (d2, r2) = double(r1)
+      val (d3, r3) = double(r2)
+      ((d1, d2, d3), r3)
+    }
+
+    def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+      import org.ababup1192.stream._
+      val list = Stream.unfold(rng.nextInt) {
+        case (n, rng: RNG) => Some(n -> rng, rng.nextInt)
+      }.take(count).toList
+      val (_, r) = list.last
+      (list.map(_._1), r)
+    }
+
+
+  }
+
+
 }
 
-case class SimpleRNG(seed: Long) extends RNG {
-  val int: Rand[Int] = _.nextInt
-
-  override def nextInt: (Int, RNG) = {
-    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-    val nextRNG = SimpleRNG(newSeed)
-    val n = (newSeed >>> 16).toInt
-    (n, nextRNG)
-  }
-}
-
-object SimpleRNG {
-  def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (n, r) = rng.nextInt
-    (if (n < 0) -(n + 1) else n, r)
-  }
-
-  def double(rng: RNG): (Double, RNG) = {
-    val (n, r) = nonNegativeInt(rng)
-    (n / (Int.MaxValue.toDouble + 2), r)
-  }
-
-  def intDouble(rng: RNG): ((Int, Double), RNG) = {
-    val (i, r) = rng.nextInt
-    val (d, r2) = double(r)
-    (i -> d, r2)
-  }
-
-  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-    val (v, r) = intDouble(rng)
-    (v.swap, r)
-  }
-
-  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-    val (d1, r1) = double(rng)
-    val (d2, r2) = double(r1)
-    val (d3, r3) = double(r2)
-    ((d1, d2, d3), r3)
-  }
-
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    import org.ababup1192.stream._
-    val list = Stream.unfold(rng.nextInt) {
-      case (n, rng: RNG) => Some(n -> rng, rng.nextInt)
-    }.take(count).toList
-    val (_, r) = list.last
-    (list.map(_._1), r)
-  }
-}
 
--- src/test/scala/org/ababup1192/StateSpec.scala
+++ src/test/scala/org/ababup1192/StateSpec.scala
@@ -1,6 +1,6 @@
 package org.ababup1192
 
-import org.ababup1192.state._
+import org.ababup1192.state.RNG._
 import org.scalatest._
 
 class StateSpec extends FlatSpec with Matchers {
@@ -14,38 +14,38 @@ class StateSpec extends FlatSpec with Matchers {
 
   "The nonNegativeInt function" should "return a (Int, RNG) value" in {
     val rng = SimpleRNG(42)
-    val (n, _) = SimpleRNG.nonNegativeInt(rng)
+    val (n, _) = rng.nonNegativeInt
     n should be(16159453)
   }
 
   "The double function" should "return a (double, RNG) value" in {
     val rng = SimpleRNG(42)
-    val (n, _) = SimpleRNG.double(rng)
+    val (n, _) = rng.double(rng)
     n should be(0.0075248316826648865)
   }
 
   "The intDouble function" should "return a ((Int, Double), RNG) value" in {
     val rng = SimpleRNG(42)
-    val ((i, d), _) = SimpleRNG.intDouble(rng)
+    val ((i, d), _) = rng.intDouble(rng)
     (i -> d) should be(16159453 -> 0.5967354846202138)
   }
 
   "The doubleInt function" should "return a ((Double, Int), RNG) value" in {
     val rng = SimpleRNG(42)
-    val ((i, d), _) = SimpleRNG.intDouble(rng)
+    val ((i, d), _) = rng.intDouble(rng)
     (i -> d) should be(16159453 -> 0.5967354846202138)
   }
 
   "The double3 function" should "return a ((Double,Double,Double), RNG) value" in {
     val rng = SimpleRNG(42)
-    val ((d1, d2, d3), _) = SimpleRNG.double3(rng)
+    val ((d1, d2, d3), _) = rng.double3(rng)
     (d1, d2, d3) should be(0.0075248316826648865,
       0.5967354846202138, 0.158467283864288)
   }
 
   "The ints function" should "return a (List[Int], RNG) value" in {
     val rng = SimpleRNG(42)
-    val (list, _) = SimpleRNG.ints(5)(rng)
+    val (list, _) = rng.ints(5)(rng)
     list should be(List(16159453, -1281479697, -340305902,
       -2015756020, 1770001318))
   }
